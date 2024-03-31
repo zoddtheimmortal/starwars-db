618f5cca9e2be9260c7cc7939964bb73e5af10be
Alok Desai <alokdesai2004@gmail.com>
2024-03-31 16:48:54 +0530

    feat: update navbar
    added page navigation to navbar

--- codebase/src/components/main_nav.tsx
+++ codebase/src/components/main_nav.tsx
@@ -1,6 +1,10 @@
 import { Component } from "solid-js";
 import Theme_Button from "./ui/theme_button";
+import { useNavigate } from "@solidjs/router";
+
 const Nav: Component<{}> = (props) => {
+	const navigate = useNavigate();
+
 	return (
 		<div class="navbar bg-base-300">
 			<div class="navbar-start">
@@ -47,22 +51,50 @@ const Nav: Component<{}> = (props) => {
 				</a>
 			</div>
 			<div class="navbar-end">
-				<button class="btn btn-ghost btn-circle">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-5 w-5"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-					>
-						<path
+				<div class="dropdown dropdown-hover">
+					<div tabindex="0" class="btn btn-ghost btn-circle">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="20"
+							height="20"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							stroke-width="2"
 							stroke-linecap="round"
 							stroke-linejoin="round"
-							stroke-width="2"
-							d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-						/>
-					</svg>
-				</button>
+							class="lucide lucide-book-open"
+						>
+							<path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
+							<path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
+						</svg>
+					</div>
+					<ul
+						tabindex="0"
+						class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+					>
+						<li>
+							<div onClick={() => navigate("/characters")}>
+								Characters
+							</div>
+						</li>
+						<li>
+							<div onClick={() => navigate("/factions")}>
+								Factions
+							</div>
+						</li>
+						<li>
+							<div onClick={() => navigate("/starships")}>
+								Starships
+							</div>
+						</li>
+						<li>
+							<div onClick={() => navigate("/planets")}>
+								Planets
+							</div>
+						</li>
+					</ul>
+				</div>
 				<Theme_Button />
 			</div>
 		</div>
